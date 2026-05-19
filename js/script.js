a778e7e192c7a3314880b696ecc8d1857b5b8c1f
Kamil <kami.okowiak2001@gmail.com>
2026-05-19 22:30:57 +0200

    added dreams section

--- js/script.js
+++ js/script.js
@@ -4,12 +4,22 @@ const allNavItems = document.querySelectorAll('.nav__item')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
+	handleNavItemsAnimation()
+}
 
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
 	allNavItems.forEach(item => {
-		item.addEventListener('click', () => {
-			nav.classList.remove('nav--active')
-		})
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
 	})
 }
 
 navBtn.addEventListener('click', handleNav)
+
+allNavItems.forEach(item => {
+	item.addEventListener('click', () => {
+		nav.classList.remove('nav--active')
+	})
+})
